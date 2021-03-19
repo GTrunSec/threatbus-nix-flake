e6e1f381c3ff04142de778396a4a7ede69150988
GTrunSec <gtrunsec@hardenedlinux.org>
2021-03-19 15:09:59 -0700

    test pyvast

--- flake.nix
+++ flake.nix
@@ -7,10 +7,12 @@
     threatbus-src = { url = "github:tenzir/threatbus"; flake = false; };
     flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
     devshell-flake.url = "github:numtide/devshell";
+    mach-nix = { url = "github:DavHau/mach-nix"; inputs.nixpkgs.follows = "nixpkgs"; };
+    vast-flake = { url = "github:GTrunSec/vast/nix-flake"; };
     nixpkgs-hardenedlinux = { url = "github:hardenedlinux/nixpkgs-hardenedlinux"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, flake-compat, devshell-flake, threatbus-src, nixpkgs-hardenedlinux }:
+  outputs = inputs@{ self, nixpkgs, flake-utils, flake-compat, devshell-flake, mach-nix, vast-flake, threatbus-src, nixpkgs-hardenedlinux }:
     { }
     //
     (flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ]
@@ -21,6 +23,8 @@
             overlays = [
               self.overlay
               devshell-flake.overlay
+              vast-flake.overlay
+              (import "${vast-flake}/nix/overlay.nix")
             ];
             config = { };
           };
@@ -31,6 +35,7 @@
             packages = [
               threatbus
               broker
+              vast
             ];
             commands = with pkgs; [
               {
@@ -57,15 +62,18 @@
             ];
           };
 
-          apps.threatbus = {
-            type = "app";
-            program = "${pkgs.threatbus}/bin/threatbus";
+          apps = {
+            threatbus = { type = "app"; program = "${pkgs.threatbus}/bin/threatbus"; };
+            threatbus-vast = { type = "app"; program = "${pkgs.threatbus-vast}/bin/pyvast-threatbus"; };
+            vast = { type = "app"; program = "${pkgs.vast}/bin/vast"; };
           };
 
           packages = inputs.flake-utils.lib.flattenTree
             rec {
               threatbus = pkgs.threatbus;
               broker = pkgs.broker;
+              threatbus-vast = pkgs.threatbus-vast;
+              vast = pkgs.vast;
             };
 
           hydraJobs = {
@@ -77,122 +85,192 @@
       )
     ) //
     {
-      overlay = final: prev: {
+      overlay = final: prev:
+        let
+          version = "2021.02.24";
 
-        broker = prev.callPackage "${nixpkgs-hardenedlinux}/pkgs/broker" { };
+          machLib = import mach-nix
+            {
+              pypiDataRev = "2205d5a0fc9b691e7190d18ba164a3c594570a4b";
+              pypiDataSha256 = "1aaylax7jlwsphyz3p73790qbrmva3mzm56yf5pbd8hbkaavcp9g";
+              python = "python38";
+            };
 
-        threatbus = with final;
-          (
-            let
-              version = "2021.02.24";
-              threatbus-zeek = python3Packages.buildPythonPackage rec {
-                pname = "zeek";
-                inherit version;
-                src = threatbus-src;
-                preConfigure = ''
-                  cd plugins/apps/threatbus_zeek
-                '';
-                doCheck = false;
-                propagatedBuildInputs = with python3Packages; [
-                  stix2
-                  stix2-patterns
-                ];
-                postPatch = ''
-                  substituteInPlace plugins/apps/threatbus_zeek/setup.py \
-                  --replace "threatbus >= 2021.2.24" ""
-                '';
-              };
+          python-packages-custom = machLib.mkPython rec {
+            requirements = ''
+              pyvast==2021.2.24
+            '';
+          };
+        in
+        {
 
-              threatbus-inmem = python3Packages.buildPythonPackage rec {
-                pname = "inmem";
-                inherit version;
-                src = threatbus-src;
-                preConfigure = ''
-                  cd plugins/backbones/threatbus_inmem
-                '';
-                doCheck = false;
-                propagatedBuildInputs = with python3Packages; [
-                  stix2
-                  stix2-patterns
-                ];
-                postPatch = ''
-                  substituteInPlace plugins/backbones/threatbus_inmem/setup.py \
-                  --replace "threatbus>=2021.2.24" ""
-                '';
-              };
+          broker = prev.callPackage "${nixpkgs-hardenedlinux}/pkgs/broker" { };
 
-              threatbus-file-benchmark = python3Packages.buildPythonPackage rec {
-                pname = "file_benchmark";
-                inherit version;
-                src = threatbus-src;
-                preConfigure = ''
-                  cd plugins/backbones/file_benchmark
-                '';
-                doCheck = false;
-                propagatedBuildInputs = with python3Packages; [
-                  stix2
-                  stix2-patterns
-                ];
-                postPatch = ''
-                  substituteInPlace plugins/backbones/file_benchmark/setup.py \
-                  --replace "threatbus>=2021.2.24" ""
-                '';
-              };
-
-              stix2-patterns = python3Packages.buildPythonPackage rec {
-                pname = "stix2-patterns";
-                version = "1.3.2";
-                src = python3Packages.fetchPypi {
-                  inherit pname version;
-                  sha256 = "sha256-F0/lMC0sMiMgUDOvmHdUEyqepFqfjgiu+vvgVJyInqQ=";
-                };
-                doCheck = false;
-                propagatedBuildInputs = with python3Packages; [
-                  antlr4-python3-runtime
-                  six
-                ];
-              };
-
-              stix2 = python3Packages.buildPythonPackage rec {
-                pname = "stix2";
-                version = "2.1.0";
-                src = python3Packages.fetchPypi {
-                  inherit pname version;
-                  sha256 = "sha256-FcnPWZ9cQxJOdv5xuIPkkY9vTPZbCExY7GS2GA9FyTg=";
-                };
-                doCheck = false;
-                propagatedBuildInputs = with python3Packages; [
-                  stix2-patterns
-                  requests
-                  pytz
-                  simplejson
-                ];
-              };
-            in
-            python3Packages.buildPythonPackage rec {
-              pname = "threatbus";
+          threatbus-vast = with final;
+            (python3Packages.buildPythonPackage {
+              pname = "threatbus_vast";
               inherit version;
               src = threatbus-src;
-
+              preConfigure = ''
+                cd apps/vast
+              '';
               doCheck = false;
-
-              propagatedBuildInputs = with python3Packages;[
+              propagatedBuildInputs = with python3Packages; [
                 stix2
+                stix2-patterns
                 confuse
-                coloredlogs
-                python-dateutil
                 black
-                pluggy
-                threatbus-zeek
-                threatbus-inmem
-                threatbus-file-benchmark
-                final.broker
+                pyzmq
+                threatbus-zmq-app
+                coloredlogs
+                python-packages-custom
+                (threatbus.overridePythonAttrs (_: {
+                  src = prev.fetchurl {
+                    url = "https://github.com/tenzir/threatbus/archive/2020.12.16.tar.gz";
+                    hash = "sha256-8hbOftFkOvuf7XG4GN2WscVDWImqDgkIhlT1VdKHFfw=";
+                  };
+                }))
               ];
               postPatch = ''
+                substituteInPlace apps/vast/setup.py \
+                --replace "threatbus >= 2020.12.16, < 2021.2.24" "" \
+                --replace "threatbus-zmq-app >= 2020.12.16, < 2021.2.24" ""
               '';
-              makeWrapperArgs = [ "--prefix PYTHONPATH : $PYTHONPATH" ];
-            }
-          );
-      };
+            });
+
+          stix2-patterns = with final;
+            (python3Packages.buildPythonPackage rec {
+              pname = "stix2-patterns";
+              version = "1.3.2";
+              src = python3Packages.fetchPypi {
+                inherit pname version;
+                sha256 = "sha256-F0/lMC0sMiMgUDOvmHdUEyqepFqfjgiu+vvgVJyInqQ=";
+              };
+              doCheck = false;
+              propagatedBuildInputs = with python3Packages; [
+                antlr4-python3-runtime
+                six
+              ];
+            });
+
+          stix2 = with final;
+            (python3Packages.buildPythonPackage rec {
+              pname = "stix2";
+              version = "2.1.0";
+              src = python3Packages.fetchPypi {
+                inherit pname version;
+                sha256 = "sha256-FcnPWZ9cQxJOdv5xuIPkkY9vTPZbCExY7GS2GA9FyTg=";
+              };
+              doCheck = false;
+              propagatedBuildInputs = with python3Packages; [
+                stix2-patterns
+                requests
+                pytz
+                simplejson
+              ];
+            });
+
+          threatbus-zmq-app = with final; (python3Packages.buildPythonPackage rec {
+            pname = "threatbus_zmq_app";
+            inherit version;
+            src = threatbus-src;
+            preConfigure = ''
+              cd plugins/apps/threatbus_zmq_app
+            '';
+            doCheck = false;
+            propagatedBuildInputs = with python3Packages; [
+              stix2
+              stix2-patterns
+              python-dateutil
+              pyzmq
+            ];
+            postPatch = ''
+              substituteInPlace plugins/apps/threatbus_zmq_app/setup.py \
+              --replace "threatbus>=2021.2.24" ""
+            '';
+          });
+
+          threatbus = with final;
+            (
+              let
+                threatbus-zeek = python3Packages.buildPythonPackage rec {
+                  pname = "threatbus_zeek";
+                  inherit version;
+                  src = threatbus-src;
+                  preConfigure = ''
+                    cd plugins/apps/threatbus_zeek
+                  '';
+                  doCheck = false;
+                  propagatedBuildInputs = with python3Packages; [
+                    stix2
+                    stix2-patterns
+                  ];
+                  postPatch = ''
+                    substituteInPlace plugins/apps/threatbus_zeek/setup.py \
+                    --replace "threatbus >= 2021.2.24" ""
+                  '';
+                };
+
+                threatbus-inmem = python3Packages.buildPythonPackage rec {
+                  pname = "threatbus_inmem";
+                  inherit version;
+                  src = threatbus-src;
+                  preConfigure = ''
+                    cd plugins/backbones/threatbus_inmem
+                  '';
+                  doCheck = false;
+                  propagatedBuildInputs = with python3Packages; [
+                    stix2
+                    stix2-patterns
+                  ];
+                  postPatch = ''
+                    substituteInPlace plugins/backbones/threatbus_inmem/setup.py \
+                    --replace "threatbus>=2021.2.24" ""
+                  '';
+                };
+
+                threatbus-file-benchmark = python3Packages.buildPythonPackage rec {
+                  pname = "threatbus_file_benchmark";
+                  inherit version;
+                  src = threatbus-src;
+                  preConfigure = ''
+                    cd plugins/backbones/file_benchmark
+                  '';
+                  doCheck = false;
+                  propagatedBuildInputs = with python3Packages; [
+                    stix2
+                    stix2-patterns
+                  ];
+                  postPatch = ''
+                    substituteInPlace plugins/backbones/file_benchmark/setup.py \
+                    --replace "threatbus>=2021.2.24" ""
+                  '';
+                };
+
+              in
+              python3Packages.buildPythonPackage rec {
+                pname = "threatbus";
+                inherit version;
+                src = threatbus-src;
+
+                doCheck = false;
+
+                propagatedBuildInputs = with python3Packages;[
+                  stix2
+                  confuse
+                  coloredlogs
+                  python-dateutil
+                  black
+                  pluggy
+                  threatbus-zeek
+                  threatbus-inmem
+                  threatbus-file-benchmark
+                  final.broker
+                ];
+                postPatch = ''
+              '';
+              }
+            );
+        };
     };
 }
